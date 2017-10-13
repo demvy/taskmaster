01099818588016dadfcaab042b63bcef5b67c5a6
Valeriy Demeshko <vdemeshk@e3r5p10.unit.ua>
2017-10-13 18:04:27 +0300

    Cmd class begin learn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env
 
 import socket
+import cmd
 
 """
 while True:
@@ -20,11 +21,73 @@ while True:
 """
 
 
-class Daemon():
-    pass
+class Daemon(cmd.Cmd):
 
-if __name__ == "__main__":
+    def do_greet(self, line):
+        print 'hello,', line
+
+    def do_EOF(self, line):
+        "Exit"
+        return True
+
+
+class Illustrate(cmd.Cmd):
+    "Illustrate the base class method use."
+
+    def cmdloop(self, intro=None):
+        print 'cmdloop(%s)' % intro
+        return cmd.Cmd.cmdloop(self, intro)
+
+    def preloop(self):
+        print 'preloop()'
+
+    def postloop(self):
+        print 'postloop()'
+
+    def parseline(self, line):
+        print 'parseline(%s) =>' % line,
+        ret = cmd.Cmd.parseline(self, line)
+        print ret
+        return ret
+
+    def onecmd(self, s):
+        print 'onecmd(%s)' % s
+        return cmd.Cmd.onecmd(self, s)
+
+    def emptyline(self):
+        print 'emptyline()'
+        return cmd.Cmd.emptyline(self)
+
+    def default(self, line):
+        print 'default(%s)' % line
+        return cmd.Cmd.default(self, line)
+
+    def precmd(self, line):
+        print 'precmd(%s)' % line
+        return cmd.Cmd.precmd(self, line)
+
+    def postcmd(self, stop, line):
+        print 'postcmd(%s, %s)' % (stop, line)
+        return cmd.Cmd.postcmd(self, stop, line)
+
+    def do_greet(self, line):
+        print 'hello,', line
+
+    def do_EOF(self, line):
+        "Exit"
+        return True
+
+
+def reading_conf_f():
     with open('taskmaster.conf', 'rw') as conf:
         for line in conf:
-            if not line.startswith(';'):
+            if line == 'config:':
                 print(line)
+    return {}
+
+if __name__ == "__main__":
+    prompt = Daemon()
+    config = reading_conf_f()
+    #prompt.logfile = config['logfile']
+    prompt.prompt = 'wtf?> '
+    prompt.cmdloop()
