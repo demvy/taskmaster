b2515043903e692661ca6e24675cc45fe0ee37a9
Oleksiy Lyubochko <olyuboch@e3r7p8.unit.ua>
2018-05-13 18:29:42 +0300

    Threading on server side. Parse args on client side. 42sh

--- client.py
+++ client.py
@@ -2,9 +2,62 @@
 
 import socket
 import sys
+import time
 
-sock = socket.socket()
-sock.connect(('localhost', 1337))
-sock.send(' '.join(sys.argv[1:]))
-data = sock.recv(1024)
-sock.close()
+commands = {
+	'start': 0,
+	'stop': 1
+}
+
+def parse_input(string):
+	if not string:
+		return 0
+
+	arg_list = string.split(" ")
+
+	try:
+		key = commands[arg_list[0]]
+	except:
+		return 0
+
+	print(key)
+
+	return 1
+
+def main():
+	sock = socket.socket()
+
+	try:
+		sock.connect(('localhost', 1337))
+	except ConnectionRefusedError as e:
+		sock.close()
+		exit()
+
+	while True:
+		try:
+			string = input('> ')
+		except KeyboardInterrupt as e:
+			break
+
+		if not string:
+			continue
+
+		if not parse_input(string):
+			continue
+
+		try:
+			sock.send(''.join([string, ]).encode())
+			data = sock.recv(1024).decode()
+		except BrokenPipeError as e:
+			break
+	
+		print(data)
+		if data == 'exit':
+			break
+
+	print('my exit')
+	sock.close()
+
+#start stop status help
+
+main()
--- server.py
+++ server.py
@@ -3,6 +3,31 @@
 import socket
 import os
 import subprocess
+import _thread
+import threading
+
+def client_entire(conn):
+    while True:
+        try:
+            data = conn.recv(1024)
+        except ConnectionResetError as e:
+            data = None
+        if not data:
+            break
+        else:
+            print(data)
+            conn.send(data)
+    conn.close()
+
+class myThread (threading.Thread):
+   def __init__(self, threadID, conn):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.conn = conn
+   def run(self):
+      print("Starting id %s" % self.threadID)
+      client_entire(self.conn)
+      print("Exiting %s" % self.threadID)
 
 
 if __name__ == "__main__":
@@ -13,17 +38,38 @@ if __name__ == "__main__":
     prompt.prompt = 'wtf?> '
     prompt.cmdloop()
     """
+    #while True:
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 1337))
+    sock.listen(5)
+
+
+    
+
+
     while True:
-        sock = socket.socket()
-        sock.bind(('', 1337))
-        sock.listen(5)
-        conn, addr = sock.accept()
+        try:
+            conn, addr = sock.accept()
+        except KeyboardInterrupt as e:
+            exit()
 
         print('connected:', addr)
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-                # conn.send(data)
-        conn.close()
+        try:
+            new_thread = myThread(1, conn)
+            new_thread.start()
+            print(new_thread.getName())
+            #new_thread.join()
+        except:
+            print ("Error: unable to start thread")
+            conn.close()
+
+        print(threading.enumerate())
+"""
+        try:
+            _thread.start_new_thread(client_entire, (conn, ))
+        except:
+            print ("Error: unable to start thread")
+            conn.close()
+"""
